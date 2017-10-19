e20b84e75cbc18b91d286aaff7d11fb1fc658af8
Tim van de Kerkhof <kerkt02@Datamining02.intra.gelre.nl>
2017-10-19 14:17:22 +0000

    this file is not in use

--- RFCPatdata.py
+++ RFCPatdata.py
@@ -3,8 +3,11 @@ import sklearn as sk
 import numpy as np
 
 def filereader(path):
-	file = pd.read_csv(path, low_memory=False, encoding="ISO-8859-1") 
+	file = pd.read_csv(path, low_memory=False, encoding="ISO-8859-1",header=0) 
 	print(file)
+
+Class rfcHandler:
+	def __init__(self, filedf)
 
 def main():
 	filereader("/home/kerkt02/patdata/QUERY_FOR_DM_ZORGACTIVITEITEN.csv")
