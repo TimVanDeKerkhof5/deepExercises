22c7db81d80e5d9efdb2d6c26d948e559bc9dbb7
Tim van de Kerkhof <kerkt02@Datamining02.intra.gelre.nl>
2017-10-31 10:08:34 +0000

    added functionality for date of birth, imputation and one-hot encoding

--- encoder.py
+++ encoder.py
@@ -3,16 +3,25 @@ import pandas as pd
 import numpy as np
 import datetime as dt
 from sklearn.preprocessing import Imputer
+data = pd.read_csv('/home/kerkt02/patdata/DM_CARDIOLOGIE.csv',header=0,low_memory=False,encoding='ISO-8859-1')
 
 def main():
-	data = pd.read_csv('/home/kerkt02/patdata/DM_CARDIOLOGIE.csv',header=0,low_memory=False,encoding='ISO-8859-1')
-	headerlist = ['DiagnoseCode', 'OpnameUitvoerder','vrgeschiedenis_maligniteit','roken']
-	for i in headerlist:
-		print(makeonehot(data[i]))
-	print(findAge(dt.datetime(1990,10,10)))
-	tsk = data['gewicht']
-	tsk[pd.isnull(tsk)] = 0
-	print(dataimputer(tsk))
+	headerlistcat = ['DiagnoseCode', 'OpnameUitvoerder','OpnameBewegingVolgnr','OpnameBehandelaar','vrgeschiedenis_myochardinfarct','vrgeschiedenis_PCI','vrgeschiedenis_CABG','vrgeschiedenis_CVA_TIA','vrgeschiedenis_vaatlijden','vrgeschiedenis_hartfalen','vrgeschiedenis_maligniteit','vrgeschiedenis_COPD','vrgeschiedenis_atriumfibrilleren','TIA','CVA_Niet_Bloedig','CVA_Bloedig','LV_Functie','dialyse','riscf_roken','riscf_familieanamnese','riscf_hypertensie','riscf_hypercholesterolemie','riscf_diabetes','roken','Radialis','Femoralis','Brachialis','vd_1','vd_2','vd_3','graftdysfunctie']
+	headerlistcon = ['Geboortedatum','lengte','gewicht','bloeddruk','HB','HT','INR','Glucose','Kreat','Trombocyten','Leukocyten','Cholesterol_totaal','Cholesterol_ldl']
+	headerlistdat = ['Geboortedatum'] #also a continuous variable, just needs to be transformed into actual integers first
+	print(len(headerlistcat)+len(headerlistcon)+len(headerlistdat))
+	trainerdict = {}
+	#onehotencode all categorical variables
+	for cat in headerlistcat:
+		trainerdict[cat] = makeonehot(data[cat])
+
+	#convert DoB into age
+	dobconverter('Geboortedatum')
+
+	for con in headerlistcon:
+		trainerdict[con] = dataimputer(con)
+
+	print(trainerdict)
 
 def makeonehot(s):
 	encoder = LabelBinarizer()
@@ -20,12 +29,21 @@ def makeonehot(s):
 	o = encoder.fit_transform(s)
 	return o
 
-def findAge(born):
-	today = dt.date.today()
-	return today.year - born.year - ((today.month, today.day) < (born.month, born.day))
+def dobconverter(l):
+	now = pd.Timestamp(dt.datetime.now())
+	data[l] = pd.to_datetime(data[l], format='%Y-%m-%d')
+	data[l] = data[l].where(data[l] < now, data[l] - np.timedelta64(100, 'Y'))
+	data[l] = (now - data[l]).astype('<m8[Y]')
+	#print(data[l])
 def dataimputer(t):
-	imputer = Imputer(missing_values=0, strategy='mean', axis=0)
-	imputer.fit_transform(t)
-	return t
+	imputer = Imputer(missing_values=np.nan, strategy='mean', axis=0)
+	c=imputer.fit_transform(data[[t]]).ravel()
+	return c
+def isnumber(x):
+	try:
+		float(x)
+		return True
+	except:
+		return False
 
 main()
